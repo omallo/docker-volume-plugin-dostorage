fb0c0d706e24add1829ffaced4b4423050a5280a
Ovidio Mallo <ovidio.mallo@gmail.com>
2016-09-30 00:07:32 +0200

    Add a build helper script to create the release binaries

---
+++ build-binaries.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+env GOOS=linux GOARCH=amd64 go build -o docker-volume-plugin-dostorage_linux_amd64
+env GOOS=linux GOARCH=386 go build -o docker-volume-plugin-dostorage_linux_386
+env GOOS=freebsd GOARCH=amd64 go build -o docker-volume-plugin-dostorage_freebsd_amd64
+env GOOS=freebsd GOARCH=386 go build -o docker-volume-plugin-dostorage_freebsd_386
